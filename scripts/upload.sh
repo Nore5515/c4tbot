2f3f6dd9a5efcd1651ee2c3ea11adfbfaa71b5fb
Noah Reed <Nore5515@gmail.com>
2024-08-23 15:44:48 -0700

    new stuff

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,1 +1,45 @@
-curl "https://catbotstorage5515.blob.core.windows.net/\$web/index.html?sp=r&st=2024-08-22T02:53:34Z&se=2024-08-22T10:53:34Z&spr=https&sv=2022-11-02&sr=c&sig=OPOFi8UtLQxuQ1sPxduPgY7j%2FeaVvMLE%2F5tDmkmWAHc%3D" --output meep.txt
+SAS="sp=raw&st=2024-08-23T21:30:49Z&se=2024-08-24T05:30:49Z&spr=https&sv=2022-11-02&sr=c&sig=%2BIqWvVXv1skolPIe%2FAAupoIYG%2Fg5Vi7qqU708kgmqnE%3D";
+HEADER="https://catbotstorage5515.blob.core.windows.net/"
+FILE="\$web/index.html"
+
+URL=$HEADER$FILE?$SAS
+
+
+ACC_NAME="catbotstorage5515"
+
+PROJ_PATH="~/projects/js/c4tbotweb";
+
+curl $URL --output meep.txt
+
+az storage blob exists \
+    --account-key $KEY \
+    --account-name $ACC_NAME \
+    --container-name \$web \
+    --name index.html
+
+# az storage blob show \
+#     --account-key $KEY \
+#     --account-name $ACC_NAME \
+#     -c \$web \
+#     -n index.html
+
+az storage blob url \
+    --account-key $KEY \
+    --account-name $ACC_NAME \
+    --container-name \$web \
+    --name index.html
+
+az storage blob download \
+    --account-key $KEY \
+    --account-name $ACC_NAME \
+    -f ~/projects/js/c4tbotweb/dl/meep.txt \
+    -c \$web \
+    -n index.html
+
+az storage blob upload \
+    --account-key $KEY \
+    --account-name $ACC_NAME \
+    --overwrite true \
+    -f $PROJ_PATH/output/index.html \
+    -c \$web \
+    -n index.html
